eb8b3c4c5c285b9e9048b2ad3dec6d416d6b832e
robin-wendel <robin.wendel@me.com>
2025-10-08 16:40:46 +0200

    Refactor paths for NetAScore profiles

--- api/paths.py
+++ api/paths.py
@@ -6,4 +6,6 @@ BASE_JOBS_DIR = PROJECT_ROOT / "jobs"
 BASE_JOBS_DIR.mkdir(parents=True, exist_ok=True)
 
 NETASCORE_DIR = PROJECT_ROOT / "netascore"
+NETASCORE_PROFILE_BIKE = NETASCORE_DIR / "examples" / "profile_bike.yml"
+NETASCORE_PROFILE_WALK = NETASCORE_DIR / "examples" / "profile_walk.yml"
 NETASCORE_SETTINGS_TEMPLATE = NETASCORE_DIR / "examples" / "settings_osm_query.yml"
