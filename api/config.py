4e19773cc58a903564ae6ff8b424ce57376da7eb
robin-wendel <robin.wendel@me.com>
2025-10-08 16:39:44 +0200

    Add API_PORT configuration

--- api/config.py
+++ api/config.py
@@ -5,8 +5,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     api_key: str
+    api_port: int = 8000
     db_host: str
-    db_port: int
+    db_port: int = 5432
     db_name: str
     db_username: str
     db_password: str
