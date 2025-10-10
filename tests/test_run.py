d446f19ba6d3d2d88431b80a67b6ae8f8d7e5fcf
robin-wendel <robin.wendel@me.com>
2025-10-10 18:33:29 +0200

    Add command-line argument for uploading netascore in test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,7 +1,14 @@
+import argparse
 from pathlib import Path
 
 from api.app import delete_old_jobs
 from pipeline.run import run_pipeline, setup_logging
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--upload-netascore", action="store_true")
+args = parser.parse_args()
+
+UPLOAD_NETASCORE = args.upload_netascore
 
 TEST_DATA_DIR = Path(__file__).resolve().parents[0] / "data"
 
@@ -11,7 +18,7 @@ setup_logging()
 def main():
     delete_old_jobs()
 
-    run_pipeline(
+    kwargs = dict(
         od_clusters_a=TEST_DATA_DIR / "b_klynger.gpkg",
         od_clusters_b=TEST_DATA_DIR / "a_klynger.gpkg",
         od_table=TEST_DATA_DIR / "Data_2023_0099_Tabel_1.csv",
@@ -24,10 +31,14 @@ def main():
         od_table_b_id_field="Arbejssted_klynge_id",
         od_table_trips_field="Antal",
         stops_id_field="stopnummer",
-        netascore_gpkg=TEST_DATA_DIR / "netascore_20251008_200432.gpkg",
         output_format="GPKG",
         seed=None,
     )
+
+    if UPLOAD_NETASCORE:
+        kwargs["netascore_gpkg"] = TEST_DATA_DIR / "netascore_20251008_200432.gpkg"
+
+    run_pipeline(**kwargs)
 
 
 if __name__ == "__main__":
